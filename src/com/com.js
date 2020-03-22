da2d277571b35d16e0828ebcbc6f0f9dd8170169
min9nim <min9nim@gmail.com>
2020-03-23 06:40:04 +0900

    update

--- src/com/com.js
+++ src/com/com.js
@@ -1,72 +1,75 @@
 function _getHostname(url) {
-  let start = url.indexOf("://") + 3;
-  let end = url.indexOf("/", start);
-  return url.slice(start, end);
+  let start = url.indexOf('://') + 3
+  let end = url.indexOf('/', start)
+  return url.slice(start, end)
 }
 
 function _getProtocol(url) {
-  let end = url.indexOf("://") + 3;
-  return url.slice(0, end);
+  let end = url.indexOf('://') + 3
+  return url.slice(0, end)
 }
 
 function sendErr(res) {
   return err => {
     res.status(200).send({
-      status: "Fail",
+      status: 'Fail',
       message: err.toString(),
-    });
-  };
+    })
+  }
 }
 
 function _bodyScrap(url) {
   return function($) {
-    const protocol = _getProtocol(url);
-    const host = _getHostname(url);
+    const protocol = _getProtocol(url)
+    const host = _getHostname(url)
     // 글제목
-    let title = $("meta[property='og:title']").attr("content");
+    let title = $("meta[property='og:title']").attr('content')
     if (!title) {
-      title = $("head title").text();
+      title = $('head title').text()
       if (!title) {
-        throw Error("This link has no title");
+        throw Error('This link has no title')
       }
     }
 
-    console.log("@@ title = " + title);
+    console.log('@@ title = ' + title)
 
     // 글이미지
-    let image = $("meta[property='og:image']").attr("content");
+    let image = $("meta[property='og:image']").attr('content')
     if (!image) {
-      image = $("img").attr("src");
+      image = $('img').attr('src')
       //이미지 세팅
-      if (image && image.indexOf("http") === 0) {
+      if (image && image.indexOf('http') === 0) {
         // http 로 시작하면 그냥 사용
-      } else if (image && image[0] === "/") {
+      } else if (image && image[0] === '/') {
         // image 경로가 / 로 시작한다면
         //let urlObj = new URL(url);
-        image = protocol + host + image;
+        image = protocol + host + image
       } else {
-        image = "";
+        image = ''
       }
     }
 
     // 파비콘
     let favicon = ''
-    let faviconPath = $('link[rel="shortcut icon"]').attr("href");
-    if(!faviconPath){
-      faviconPath = $('link[rel="mask-icon"]').attr("href");
+    let faviconPath = $('link[rel="shortcut icon"]').attr('href')
+    if (!faviconPath) {
+      faviconPath = $('link[rel="mask-icon"]').attr('href')
     }
-    if(faviconPath.startsWith('http')){
+    if (!faviconPath) {
+      faviconPath = $('link[rel="icon"]').attr('href')
+    }
+    if (faviconPath.startsWith('http')) {
       favicon = faviconPath
-    }else{
-      favicon = faviconPath.startsWith("//")
-      ? protocol + faviconPath.slice(2)
-      : protocol + host + faviconPath    
+    } else {
+      favicon = faviconPath.startsWith('//')
+        ? protocol + faviconPath.slice(2)
+        : protocol + host + faviconPath
     }
-    
+
     // 글요약본
-    let desc = $("meta[property='og:description']").attr("content");
+    let desc = $("meta[property='og:description']").attr('content')
     if (!desc) {
-      desc = "";
+      desc = ''
     }
 
     return {
@@ -74,64 +77,64 @@ function _bodyScrap(url) {
       image,
       desc,
       favicon,
-    };
-  };
+    }
+  }
 }
 //_bodyScrap = $ => _bodyScrap($, _getProtocol, _getHostname);
 
 const timelog = (function() {
-  let time = [];
+  let time = []
   let o = {
     start: function() {
-      time = [Date.now()];
+      time = [Date.now()]
       console.log(
-        "[timelog] start: " + new Date(time[0]).toString().substr(0, 24),
-      );
+        '[timelog] start: ' + new Date(time[0]).toString().substr(0, 24),
+      )
     },
     check: function() {
-      time.push(Date.now());
-      let diff = time[time.length - 1] - time[time.length - 2];
-      console.log("[timelog] + " + diff + "ms");
+      time.push(Date.now())
+      let diff = time[time.length - 1] - time[time.length - 2]
+      console.log('[timelog] + ' + diff + 'ms')
     },
-  };
-  return o;
-})();
+  }
+  return o
+})()
 
 const myRedis = (function() {
-  let cache = {};
+  let cache = {}
 
   let o = {
     get: function(key) {
-      return cache[key];
+      return cache[key]
     },
     set: function(key, val) {
-      return (cache[key] = val);
+      return (cache[key] = val)
     },
     clear: function() {
-      console.log("@@ redis cache 초기화");
-      cache = {};
+      console.log('@@ redis cache 초기화')
+      cache = {}
     },
-  };
-  return o;
-})();
+  }
+  return o
+})()
 
 function redisWrapper(fn) {
   return async function(cond, idx, cnt) {
-    timelog.start();
-    let key = JSON.stringify({ cond, idx, cnt });
-    let cache = myRedis.get(key);
+    timelog.start()
+    let key = JSON.stringify({ cond, idx, cnt })
+    let cache = myRedis.get(key)
     if (cache) {
-      timelog.check();
-      console.log("return cache [key] = " + key);
-      return cache;
+      timelog.check()
+      console.log('return cache [key] = ' + key)
+      return cache
     } else {
-      let res = await fn(cond, idx, cnt);
-      myRedis.set(key, res);
-      timelog.check();
-      console.log("cache added [key] = " + key);
-      return res;
+      let res = await fn(cond, idx, cnt)
+      myRedis.set(key, res)
+      timelog.check()
+      console.log('cache added [key] = ' + key)
+      return res
     }
-  };
+  }
 }
 
 /**
@@ -150,15 +153,15 @@ function redisWrapper(fn) {
 // }
 
 function clearRedisWhenCUD(req, res, next) {
-  if (req.method !== "GET") {
-    myRedis.clear();
+  if (req.method !== 'GET') {
+    myRedis.clear()
   }
-  next();
+  next()
 }
 
 function isExpired(exp) {
-  let month = 1000 * 60 * 60 * 24 * 30;
-  return Date.now() > exp + month; // 만료시간을 임의로 한달 연장
+  let month = 1000 * 60 * 60 * 24 * 30
+  return Date.now() > exp + month // 만료시간을 임의로 한달 연장
 }
 
 module.exports = {
@@ -171,4 +174,4 @@ module.exports = {
   redisWrapper,
   clearRedisWhenCUD,
   isExpired,
-};
+}
