0496ca039405de2d2456b2f244bc83cb7121c7c0
min9nim <min9nim@gmail.com>
2020-06-06 17:46:49 +0900

    update log

--- src/router/webscrap-router-def.js
+++ src/router/webscrap-router-def.js
@@ -1,26 +1,27 @@
-const webscrap = require("../com/webcrawler");
-const {sendErr} = require("../com/com")
+const webscrap = require('../com/webcrawler')
+const { sendErr } = require('../com/com')
 
-const post = {};
-const get = {};
-const put = {};
-const del = {};
+const post = {}
+const get = {}
+const put = {}
+const del = {}
 
 const cache = {}
 
 post['/'] = async (req, res) => {
-  console.log("req.body.url = " + req.body.url);
+  const $l = global.$logger
+  $l.verbose('req.body.url = ' + req.body.url)
   try {
-    console.log({cache})
-    if(cache[req.body.url]){
+    console.log({ cache })
+    if (cache[req.body.url]) {
       console.log('[hit]', req.body.url)
       res.jsonp(cache[req.body.url])
       return
     }
     console.log('[no-hit]', req.body.url)
-    let result = await webscrap(req.body.url);
+    let result = await webscrap(req.body.url)
     cache[req.body.url] = result
-    res.jsonp(result);
+    res.jsonp(result)
   } catch (e) {
     sendErr(res)(e)
     // res.jsonp({
@@ -28,6 +29,5 @@ post['/'] = async (req, res) => {
     // });
   }
 }
-
 
 module.exports = { post, get, put, del }
